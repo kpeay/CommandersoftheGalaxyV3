1c6cbc22cb6be1bd7b48c0b07d7e1bb7c8c91120
Mehmet Soner Calis <mscalis@uncc.edu>
2018-12-01 11:54:47 -0500

    Start Panel Additions

--- Assets/StartPanel/OptionsMenu.cs
+++ Assets/StartPanel/OptionsMenu.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour {
+
+    public Toggle playerTank1;
+    public Toggle playerSoldier1;
+    public Toggle playerRange1;
 
     // Human Race Selected
     public void HumansButtonCLicked()
     {
+        
         Debug.Log("Humans Button Clicked");
     }
 
