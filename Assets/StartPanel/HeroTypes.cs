deb2e2a8f0354f4598827b58809f58d457cebcc7
Mehmet Soner Calis <mscalis@uncc.edu>
2018-12-01 18:20:31 -0500

    Added hero type selection option
    From options menu you can select the hero type for player and enemy NPC/AI. This will set one of the hero type boolean variable.
    The bool variable names you can refer is as follows:
    HeroTypes.isPlayerHeroAttackType
    HeroTypes.isPlayerHeroDefenseType
    HeroTypes.isPlayerHeroRangeType
    
    HeroTypes.isEnemyHeroAttackType
    HeroTypes.isEnemyHeroDefenseType
    HeroTypes.isEnemyHeroRangeType
    
    You can use these bool variables to increase attack/defense/range parameters of their unit.

---
+++ Assets/StartPanel/HeroTypes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroTypes : MonoBehaviour {
+
+    public static bool isPlayerHeroAttackType {get; set;}
+    public static bool isPlayerHeroDefenseType {get; set;}
+    public static bool isPlayerHeroRangeType {get; set;}
+
+    public static bool isEnemyHeroAttackType {get; set; }
+    public static bool isEnemyHeroDefenseType {get; set; }
+    public static bool isEnemyHeroRangeType {get; set; }
+
+    private void Start()
+    {
+        HeroTypes.isPlayerHeroAttackType = true;
+        HeroTypes.isPlayerHeroDefenseType = false;
+        HeroTypes.isPlayerHeroRangeType = false;
+
+        HeroTypes.isEnemyHeroAttackType = true;
+        HeroTypes.isEnemyHeroDefenseType = false;
+        HeroTypes.isEnemyHeroDefenseType = false;
+        Debug.Log("Player and Enemy Hero Type Set To ATTACK....");
+    }
+
+}
--- Assets/StartPanel/OptionsMenu.cs
+++ Assets/StartPanel/OptionsMenu.cs
@@ -6,26 +6,56 @@ using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour {
 
-    public Toggle playerTank1;
-    public Toggle playerSoldier1;
-    public Toggle playerRange1;
+    // Player Hero Types
+    public Toggle isPlayerHeroAttack;
+    public Toggle isPlayerHeroDefense;
+    public Toggle isPlayerHeroRange;
 
-    // Human Race Selected
-    public void HumansButtonCLicked()
-    {
-        
-        Debug.Log("Humans Button Clicked");
+    // Enemy Hero Types
+    public Toggle isEnemyHeroAttack;
+    public Toggle isEnemyHeroDefense;
+    public Toggle isEnemyHeroRange;
+
+    // Player Hero Attack Type Changed
+    public void PlayerHeroTypeAttackChanged()
+    {      
+        HeroTypes.isPlayerHeroAttackType = isPlayerHeroAttack.isOn;
+        Debug.Log("Player Hero Attack Type Changed To " + isPlayerHeroAttack.isOn);
     }
 
-    // Gungan Race Selected
-    public void GungansButtonCLicked () {
-        Debug.Log("Gungans Button Clicked");
-	}
-
-    // Wookies Race Selected
-    public void WookiesButtonCLicked()
+    // Player Hero Defense Type Changed
+    public void PlayerHeroTypeDefenseChanged()
     {
-        Debug.Log("Wookies Button Clicked");
+        HeroTypes.isPlayerHeroDefenseType = isPlayerHeroDefense.isOn;
+        Debug.Log("Player Hero Defense Type Changed To " + isPlayerHeroDefense.isOn);
+    }
+
+    // Player Hero Range Type Changed
+    public void PlayerHeroTypeRangeChanged()
+    {
+        HeroTypes.isPlayerHeroRangeType = isPlayerHeroRange.isOn;
+        Debug.Log("Player Hero Range Type Changed To " + isPlayerHeroRange.isOn);
+    }
+
+    // Enemy Hero Attack Type Changed
+    public void EnemyHeroTypeAttackChanged()
+    {
+        HeroTypes.isEnemyHeroAttackType = isEnemyHeroAttack.isOn;
+        Debug.Log("Enemy Hero Attack Type Changed To " + isEnemyHeroAttack.isOn);
+    }
+
+    // Enemy Hero Defense Type Changed
+    public void EnemyHeroTypeDefenseChanged()
+    {
+        HeroTypes.isEnemyHeroDefenseType = isEnemyHeroDefense.isOn;
+        Debug.Log("Enemy Hero Defense Type Changed To " + isEnemyHeroDefense.isOn);
+    }
+
+    // Enemy Hero Range Type Changed
+    public void EnemyHeroTypeRangeChanged()
+    {
+        HeroTypes.isEnemyHeroRangeType = isEnemyHeroRange.isOn;
+        Debug.Log("Enemy Hero Range Type Changed To " + isEnemyHeroRange.isOn);
     }
 
 }
