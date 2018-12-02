3b9071802b9553cefa9e6b0801c48dcbbaf8e43f
Mehmet Soner Calis <mscalis@uncc.edu>
2018-12-02 18:38:11 -0500

    WinScene update

---
+++ Assets/Resources/WinScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WinScene : MonoBehaviour {
+
+    // Start game playing
+    public void BackButtonClicked()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
