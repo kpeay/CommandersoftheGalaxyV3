1ea39ee29fb0279051c7e6632e41c2f84f7f7227
Mehmet Soner Calis <mscalis@uncc.edu>
2018-12-02 19:42:23 -0500

    GameOver Scene Updated

---
+++ Assets/GameOverScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScene : MonoBehaviour {
+
+    // Start game playing
+    public void BackButtonClicked()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
